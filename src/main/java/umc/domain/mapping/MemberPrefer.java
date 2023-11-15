3d6ea20b74a06175398b2f98141612089f8fdc3a
sh <dldh0917@naver.com>
2023-11-15 16:45:27 +0900

    미션 제출

---
+++ src/main/java/umc/domain/mapping/MemberPrefer.java
@@ -0,0 +1,28 @@
+package umc.domain.mapping;
+
+import lombok.*;
+import umc.domain.FoodCategory;
+import umc.domain.Member;
+import umc.domain.base.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberPrefer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private FoodCategory foodCategory;
+}
